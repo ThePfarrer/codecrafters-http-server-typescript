c7fff77ce1a7d26ac3af3ca68b3e792d4d29892f
Uche Okereke <orezpablo@gmail.com>
2026-01-16 15:13:09 +0100

    write http 200

--- app/main.ts
+++ app/main.ts
@@ -4,8 +4,9 @@ import * as net from "net";
 console.log("Logs from your program will appear here!");
 
 // TODO: Uncomment the code below to pass the first stage
-const server = net.createServer((socket) => {
-  socket.on("close", () => {
+const server = net.createServer((socket: net.Socket) => {
+  socket.on("data", () => {
+    socket.write(`HTTP/1.1 200 OK\r\n\r\n`);
     socket.end();
   });
 });
